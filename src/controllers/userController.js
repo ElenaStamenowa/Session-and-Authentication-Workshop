461a41266b4bd71d75cfca97bd9b20eae319e7c4
ElenaStamenowa <122873905+ElenaStamenowa@users.noreply.github.com>
2024-01-14 09:54:22 +0200

    implement login logic with bcrypt comparing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,13 +9,24 @@ router.post("/register", async (req, res) => {
   // console.log({ inputData: req.body });
   const { username, password, repeatPassword } = req.body;
 
-  await userService.register({username, password, repeatPassword});
+  await userService.register({ username, password, repeatPassword });
 
   res.redirect("/users/login");
 });
 
 router.get("/login", (req, res) => {
   res.render("user/login");
+});
+
+router.post("/login", async (req, res) => {
+  //find the user
+  const { username, password } = req.body;
+  //console.log(username, " ", password);
+
+  const user = await userService.login(username, password);
+  //compare passwords
+
+  res.redirect("/");
 });
 
 module.exports = router;
--- src/service/userService.js
+++ src/service/userService.js
@@ -1,9 +1,26 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-//3 - layer architecture
-//controller -> service ->domain model
+//3 - Layer Architecture
+//controller -> service -> domain model
 //if(rPass === pass)
 
 exports.register = (userData) => {
   return User.create(userData);
+};
+
+exports.login = async (username, password) => {
+  const user = await User.findOne({ username });
+  //validate username
+  if (!user) {
+    throw new Error("Invalid username or password");
+  }
+
+  //validate password
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    throw new Error("Invalid username or password");
+  }
+
+  return user;
 };
